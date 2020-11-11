ba2d0118012b8321a3b0b35cc35b8c7295da9f4f
Alexander Pavlovich <glazynovexceedteam@gmail.com>
2020-11-11 16:10:06 +0300

    update

--- products/users.js
+++ products/users.js
@@ -1,8 +1,7 @@
 const {Users} = require('../modules');
 
 exports.newUser = (req, res) => {
-    Users.find({name: req.body.name}, (err, client) => {
-
+    Users.find({name: req.body.name,password:req.body.password}, (err, client) => {
         if(err) return  next(err)
        else if(client.length !== 0) {
             return res.send({ client, status:'cannot create an existing user'})
@@ -22,7 +21,7 @@ exports.newUser = (req, res) => {
 
 }
 exports.user = (req, res)  => {
-    Users.find({name:req.query.name} ,(err, user) => {
+    Users.find({name:req.query.name,password:req.query.password} ,(err, user) => {
         if(err) return  next(err)
 
         res.send({user, status: "there is such a user"})
